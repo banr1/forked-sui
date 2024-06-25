ff167adc61cd2e46b6d2ca102d136fc2434dfa86
Ashok Menon <ashok@mystenlabs.com>
2024-06-25 23:20:49 +0100

    Factor out configs, add explorer URLs

---
+++ examples/tic-tac-toe/ui/src/config.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { createNetworkConfig } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+
+const { networkConfig, useNetworkVariable } = createNetworkConfig({
+  localnet: {
+    url: getFullnodeUrl("localnet"),
+    variables: {
+      explorer: (id: string) => `https://suiscan.xyz/custom/object/${id}/?network=0.0.0.0%3A9000`,
+    },
+  },
+  devnet: {
+    url: getFullnodeUrl("devnet"),
+    variables: {
+      explorer: (id: string) => `https://suiscan.xyz/devnet/object/${id}/`,
+    },
+  },
+  testnet: {
+    url: getFullnodeUrl("testnet"),
+    variables: {
+      explorer: (id: string) => `https://suiscan.xyz/testnet/object/${id}/`,
+    },
+  },
+});
+
+export { networkConfig, useNetworkVariable };
